233f3f850333db5d1bc63eccc7c8206a91974ac2
Phongsakorn <s6603052411032@email.kmutnb.ac.th>
2025-01-09 11:18:56 +0700

    Create func-component.js And Update App.js
    Create func-component.js And Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import './style.css'
+// import './style.css'
+import { Header,Content,Footer } from './func-components';
 
 function App() {
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
+      {/* <h2>DeveloperThai.com</h2>
+      <div className='title'>React & React Native</div> */}
+      <Header/>
+      <Content/>
+      <Footer/>
     </>
   )
 }
